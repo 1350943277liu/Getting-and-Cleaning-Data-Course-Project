7cad8f927860ceed466f3b660647b58e5bc1abed
刘学诚 <1350943277liu@gmail.com>
2020-07-12 19:59:45 +0800

    complete the script

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,16 @@
 library(dplyr)
 library(tidyr)
 
+## Download and unzip the dataset if necessary
+if (!dir.exists("UCI HAR Dataset")) {
+        if (!file.exists("getdata-projectfiles-UCI HAR Dataset.zip")) {
+                url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+                download.file(url, "./getdata-projectfiles-UCI HAR Dataset.zip", method = "curl")
+        }
+        unzip("getdata-projectfiles-UCI HAR Dataset.zip")
+}
+
+# Load the train and the test sets
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
 activity_labels <- tolower(activity_labels)
 features <- read.table("./UCI HAR Dataset/features.txt")[,2]
@@ -13,7 +23,7 @@ subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
 
-## Merges the training and the test sets to create one data set
+## Merge the train and the test sets to create one data set
 subject_test <- mutate(subject_test, usage ="test")
 subject_train <- mutate(subject_train, usage ="test")
 
@@ -25,12 +35,12 @@ names(activities) <- c("subject", "usage", "activity", features)
 
 activities <- as_tibble(activities, .name_repair = "minimal")
 
-## Extracts only the measurements on the mean and standard deviation for each measurement
+## Extract only the measurements on the mean and standard deviation for each measurement
 activities <- activities %>%
         select(1:3, contains("mean"), contains("std")) %>%
         pivot_longer(cols = 4:89, names_to = "measurement", values_to = "value")
 
-# Converts "usage" and "activity" as factor
+# Convert "usage" and "activity" as factor
 activities$activity <- activity_labels[activities$activity]
 activities$activity <- factor(activities$activity, levels = activity_labels)
 
