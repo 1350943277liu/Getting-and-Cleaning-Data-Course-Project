0dbde521a0075e776990cbefdc6f738f3675565e
刘学诚 <1350943277liu@gmail.com>
2020-07-12 08:41:01 +0800

    read and combine raw data

--- run_analysis.R
+++ run_analysis.R
@@ -8,5 +8,16 @@ subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
 
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
 
-test <- cbind(subject_test, X_test, Y_test)
+subject_test <- mutate(subject_test, usage ="test")
+subject_train <- mutate(subject_train, usage ="test")
+
+
+test <- cbind(subject_test, Y_test, X_test)
+train <- cbind(subject_train, Y_train, X_train)
+
+activities <- rbind(test, train)
+names(activities) <- c("subject", "usage", "activity", features)
